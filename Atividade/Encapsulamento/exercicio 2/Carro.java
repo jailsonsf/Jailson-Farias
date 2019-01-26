3ae269b6f0794d7366039b61b427aa94f0ab961b
jailsonsf <jailsonsoares002@gmail.com>
2019-01-26 12:41:37 -0300

    Exercício 2 Encapsulamento :heavy_check_mark:

---
+++ Atividade/Encapsulamento/exercicio 2/Carro.java
@@ -0,0 +1,46 @@
+package Encapsulamento.exercicio2;
+
+public class Carro
+{
+
+    private String marca;
+    private String cor;
+    private String ano;
+
+    public Carro ( String marca, String cor, int ano)
+    {
+        setMarca (marca);
+        setCor (cor);
+        setAno (ano);
+    }
+
+    public void setMarca ( String marca)
+    {
+        this.marca = marca;
+    }
+
+    public void setCor ( String cor)
+    {
+        this.cor = cor;
+    }
+
+    public void setAno ( int ano)
+    {
+        this.ano = ano;
+    }
+
+    public String getNome ()
+    {
+        return nome;
+    }
+
+    public String getCor ()
+    {
+        return cor;
+    }
+
+    public int getAno ()
+    {
+        return ano;
+    }
+}
