b23b3c15f53582663161e9a64e58fb939ebbada3
jailsonsf <jailsonsoares002@gmail.com>
2019-02-01 20:41:17 -0300

    Update project :rocket:

--- springBoot/booksale/src/main/java/com/booksale/domain/BookPublisher.java
+++ springBoot/booksale/src/main/java/com/booksale/domain/BookPublisher.java
@@ -1,4 +1,4 @@
-package com.projectp2lp2.domain;
+package com.booksale.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- springBoot/booksale/src/main/java/com/booksale/repository/AuthorRepository.java
+++ springBoot/booksale/src/main/java/com/booksale/repository/AuthorRepository.java
@@ -1,8 +1,8 @@
-package com.projectp2lp2.repository;
+package com.booksale.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.projectp2lp2.domain.Author;
+import com.booksale.domain.Author;
 
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
 
