93d04b3609467c2f2a56bc0b5cdddefa28347924
jailsonsf <jailsonsoares002@gmail.com>
2019-02-02 16:53:43 -0300

    Update :rockeet:

--- springBoot/booksale/src/main/java/com/booksale/resources/AuthorResources.java
+++ springBoot/booksale/src/main/java/com/booksale/resources/AuthorResources.java
@@ -1,19 +1,30 @@
 package com.booksale.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import com.booksale.domain.Author;
+import com.booksale.domain.Book;
 import com.booksale.repository.AuthorRepository;
+import com.booksale.repository.BookRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class AuthorResources
 {
     @Autowired
     private AuthorRepository repository;
+
+    @Autowired
+    private BookRepository bookRepository;
 
     @GetMapping("/authors")
     public List<Author> getAllAuthor ()
@@ -22,4 +33,29 @@ public class AuthorResources
 
         return authors;
     }
+
+    @PostMapping("/authors")
+    public ResponseEntity<?> saveAuthor (@RequestBody Author author)
+    {
+        Author savedAuthor = repository.save(author);
+
+        List<Book> books = savedAuthor.getAllBooks();
+        savedAuthor.setBooks(books);
+
+        for (Book eachBooks : books)
+        {
+            eachBooks.setAuthor(savedAuthor);
+            bookRepository.save(eachBooks);
+        }
+
+        URI uri = ServletUriComponentsBuilder.
+            fromCurrentRequest().
+            path("/{id}").
+            buildAndExpand(savedAuthor.getId()).
+            toUri();
+
+        return ResponseEntity.created(uri).body("Nome e Id do novo autor: " + 
+            savedAuthor.getName() + " - " + savedAuthor.getId());
+    }
+    
 }
