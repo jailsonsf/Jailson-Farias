b57e598a29dd9eecc9676ab8f7ea15c83d3e0c65
jailsonsf <jailsonsoares002@gmail.com>
2019-02-02 09:30:49 -0300

    Update project :rocket:

---
+++ springBoot/booksale/src/main/java/com/booksale/domain/Book.java
@@ -0,0 +1,52 @@
+package com.booksale.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book
+{
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+    private String description;
+
+    public Book (){}
+
+    public Book (String title, String description)
+    {
+        super();
+        setTitle (title);
+        setDescription (description);
+    }
+
+    public Integer getId ()
+    {
+        return id;
+    }
+
+    public void setTitle (String title)
+    {
+        this.title = title;
+    }
+
+    pubic String getTitle ()
+    {
+        return title;
+    }
+
+    public void setDescription (String description)
+    {
+        this.description = description;
+    }
+
+    public String getDescription ()
+    {
+        return description;
+    }
+
+}
