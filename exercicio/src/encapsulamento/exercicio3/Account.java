195a5f020d6a903daa07eb47a16d94f93c8bf2a4
jailsonsf <jailsonsoares002@gmail.com>
2019-02-03 12:26:20 -0300

    refactoring :hammer:

---
+++ exercicio/src/encapsulamento/exercicio3/Account.java
@@ -0,0 +1,55 @@
+package encapsulamento.exercicio3;
+
+public class Account
+{
+    public Account ( int numberAccount)
+    {
+        setNumberAccount(numberAccount);
+    }
+
+    private int numberAccount;
+    private float balance;
+
+    public void setNumberAccount ( int numberAccount)
+    {
+        this.numberAccount = numberAccount;
+    }
+
+    public int getNumberAccount()
+    {
+        return numberAccount;
+    }
+
+    public float getBalance()
+    {
+        return balance;
+    }
+
+    private boolean validCashWithdrawal ( float value )
+    {
+        return balance >= this.balance;
+    }
+
+    private boolean validAccount (int number)
+    {
+        return this.numberAccount == number;
+    }
+
+    public void deposit ( int number, float value)
+    {
+
+        if ( validAccount(number))
+        {
+            this.balance += value;
+        }
+    }
+
+    public void cashWithdrawal ( int number, float value)
+    {
+        if ( validAccount(number) && validCashWithdrawal(value))
+        {
+            this.balance -= value;
+        }
+    }
+
+}
