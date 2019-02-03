4914870e737a57ad0b0b3d821dc67206597b9def
jailsonsf <jailsonsoares002@gmail.com>
2019-02-03 17:24:35 -0300

    refactoring :heavy_check_mark:

--- exercicio/src/polimorfismo/exercicio1/Main.java
+++ exercicio/src/polimorfismo/exercicio1/Main.java
@@ -5,10 +5,10 @@ public class Main
     public static void main (String[] args)
     {
 
-        Funcionario f = new Funcionario("juquinha", "123.456.789-10", "juquinha@gmail.com", 1, "01/02/1988", "vendedor", "juquinha123");
+        Employee f = new Employee("juquinha", "123.456.789-10", "juquinha@gmail.com", 1, "01/02/1988", "vendedor", "juquinha123");
 
-        System.out.println(f.realizarLogin("juquinha@gmail.com", "juquinha123"));
+        System.out.println(f.Login("juquinha@gmail.com", "juquinha123"));
 
-        System.out.println(f.realizarLogin(1, "juquinha123"));
+        System.out.println(f.Login(1, "juquinha123"));
     }
 }
